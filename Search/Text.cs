9600a3fe31a5dba0d90e552e2a38b9bf371b205e
XiaoHuanwise <1056366209@qq.com>
2018-12-12 22:52:11 +0800

    新建工程文件；测试了弹出搜索网站。

---
+++ Search/Text.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Search
+{
+	static class Text
+	{
+		public static void Web(string keyword)
+		{
+			System.Diagnostics.Process.Start("https://www.baidu.com/s?ie=UTF-8&wd="+keyword);
+			return;
+		}
+
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("请输入要搜索的内容：");
+			Web(Console.ReadLine());
+			return;
+		}
+	}
+}
---
+++ TEXT/Search.cs
@@ -0,0 +1,17 @@
+﻿namespace TEXT
+{
+	class Search
+	{
+		public static void Web(string keyword)
+		{
+			System.Diagnostics.Process.Start("https://www.baidu.com/s?ie=UTF-8&wd="+keyword);
+			return;
+		}
+
+		public static void Main(string[] args)
+		{
+			Web("比利王");
+			return;
+		}
+	}
+}
