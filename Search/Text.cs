ada05235e2703736df1da313ac854fa9e0490739
XiaoHuanwise <1056366209@qq.com>
2018-12-13 13:27:39 +0800

    添加了打开术语翻译网站和多网站选项的功能

--- Search/Text.cs
+++ Search/Text.cs
@@ -4,16 +4,23 @@ namespace Search
 {
 	static class Text
 	{
-		public static void Web(string keyword)
+		public static void Web(string keyword, short num)
 		{
-			System.Diagnostics.Process.Start("https://www.baidu.com/s?ie=UTF-8&wd="+keyword);
+			string[] url = 
+				{
+					"https://www.baidu.com/s?ie=UTF-8&wd=", 
+					"http://dict.cnki.net/dict_result.aspx?searchword="
+				};
+			System.Diagnostics.Process.Start(url[num] + keyword);
 			return;
 		}
 
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("请输入要搜索的内容：");
-			Web(Console.ReadLine());
+			string KW = Console.ReadLine();
+			Web(KW, 0);
+			Web(KW, 1);
 			return;
 		}
 	}
