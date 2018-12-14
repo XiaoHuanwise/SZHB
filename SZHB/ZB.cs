fbce60271529909633fec2c52e05e60a565e8bc0
XiaoHuanwise <1056366209@qq.com>
2018-12-14 18:25:09 +0800

    添加了查错和注释

--- SZHB/ZB.cs
+++ SZHB/ZB.cs
@@ -12,38 +12,57 @@ namespace SZHB
 	{
 		public static void Main(string[] args)
 		{
-			string ret = HttpPost("https://webapi.ccash.com/common/rate", "32");
-			//string[] sArray=ret.Split(new char[2] {'z','}'});
-			//foreach (string i in sArray) Console.WriteLine(i.ToString() + "<br>");
+			var ret = HttpPost("https://webapi.ccash.com/common/rate", "32");//从CCASH获取更新数据包
+			if (ret == "读取数据包失败")
+			{
+				Console.WriteLine("读取数据包错误！！！");
+			}//检查是否出错
+			//截取数据包的zh_CN部分-----------------
 			ret = ret.Remove(0, ret.Length - 445);
 			ret = ret.Remove(ret.Length - 4, 3);
-			Console.WriteLine(ret);
+			//------------------------------------
+			
+			Console.WriteLine(ret);//输出检查
+			return;
 		}
 		
-		private static CookieContainer cookie = new CookieContainer();
-		
-		private static string HttpPost(string url, string postDataStr)
+		//POST请求部分----------------------------------------------------------------------------
+		private static CookieContainer cookie = new CookieContainer();//新建一个cookie
+		private static string HttpPost(string url, string postDataStr)//http模拟请求函数(XRH)
 		{
-			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-			request.Method = "POST";
-			request.ContentType = "application/json;charset=UTF-8";
-			request.ContentLength = Encoding.UTF8.GetByteCount(postDataStr);
-			request.CookieContainer = cookie;
-			Stream myRequestStream = request.GetRequestStream();
-			StreamWriter myStreamWriter = new StreamWriter(myRequestStream, Encoding.GetEncoding("gb2312"));
-			myStreamWriter.Write(postDataStr);
-			myStreamWriter.Close();
+			try//尝试，如果出错就处理
+			{
+				HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+				//从head匹配相关设置
+				request.Method = "POST";//请求类型为POST
+				request.ContentType = "application/json;charset=UTF-8";
+				request.ContentLength = Encoding.UTF8.GetByteCount(postDataStr);
+				request.CookieContainer = cookie;
+				Stream myRequestStream = request.GetRequestStream();
+				StreamWriter myStreamWriter = new StreamWriter(myRequestStream, Encoding.GetEncoding("gb2312"));
+				myStreamWriter.Write(postDataStr);
+				myStreamWriter.Close();
  
-			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+				HttpWebResponse response = (HttpWebResponse)request.GetResponse();
  
-			response.Cookies = cookie.GetCookies(response.ResponseUri);
-			Stream myResponseStream = response.GetResponseStream();
-			StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
-			string retString = myStreamReader.ReadToEnd();
-			myStreamReader.Close();
-			myResponseStream.Close();
+				response.Cookies = cookie.GetCookies(response.ResponseUri);
+				Stream myResponseStream = response.GetResponseStream();
+				StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+				string retString = myStreamReader.ReadToEnd();
+				myStreamReader.Close();
+				myResponseStream.Close();
  
-			return retString;
+				return retString;
+			}
+			catch (Exception e)
+			{
+				//Console.WriteLine(e);
+			}
+
+			return "读取数据包失败";//如果在try中没有返回，则返回错误信息
 		}
+		//---------------------------------------------------------------------------------------
+		
+		
 	}
 }
