f36ec42bc774b3b506b995e1dfb43ad87a4dcb48
XiaoHuanwise <1056366209@qq.com>
2018-12-14 14:18:47 +0800

    成功读到XRH中的rate，并且分割出了zh的部分，但是zh中的数据好像有点不对，有待进一步研究！

--- SZHB/ZB.cs
+++ SZHB/ZB.cs
@@ -1,18 +1,49 @@
-﻿namespace SZHB
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SZHB
 {
 	/*internal*/ static class Zb
 	{
 		public static void Main(string[] args)
 		{
+			string ret = HttpPost("https://webapi.ccash.com/common/rate", "32");
+			//string[] sArray=ret.Split(new char[2] {'z','}'});
+			//foreach (string i in sArray) Console.WriteLine(i.ToString() + "<br>");
+			ret = ret.Remove(0, ret.Length - 445);
+			ret = ret.Remove(ret.Length - 4, 3);
+			Console.WriteLine(ret);
 		}
-	}
-	
-	public class CCASH
-	{
-		public int CC { get; set; }
-		public int CA { get; set; }
-		public int XC { get; set; }
-		public int CWV { get; set; }
-		public int USDT { get; set; }
+		
+		private static CookieContainer cookie = new CookieContainer();
+		
+		private static string HttpPost(string url, string postDataStr)
+		{
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+			request.Method = "POST";
+			request.ContentType = "application/json;charset=UTF-8";
+			request.ContentLength = Encoding.UTF8.GetByteCount(postDataStr);
+			request.CookieContainer = cookie;
+			Stream myRequestStream = request.GetRequestStream();
+			StreamWriter myStreamWriter = new StreamWriter(myRequestStream, Encoding.GetEncoding("gb2312"));
+			myStreamWriter.Write(postDataStr);
+			myStreamWriter.Close();
+ 
+			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+ 
+			response.Cookies = cookie.GetCookies(response.ResponseUri);
+			Stream myResponseStream = response.GetResponseStream();
+			StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+			string retString = myStreamReader.ReadToEnd();
+			myStreamReader.Close();
+			myResponseStream.Close();
+ 
+			return retString;
+		}
 	}
 }
